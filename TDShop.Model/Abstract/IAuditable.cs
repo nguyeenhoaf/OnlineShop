0466878e9ab8f8cecafbefdd263becf70aeab575
nguyenhoa <nguyenhoajt@gmail.com>
2018-07-03 15:27:50 +0700

    Model first

---
+++ TDShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TDShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime? UpdatedDate { get; set; }
+        string UpdatedBy { get; set; }
+
+        //Seoable
+        string MetaKeyword { get; set; }
+        string MetaDescription { get; set; }
+
+        //Switchable
+        bool Status { get; set; }
+    }
+}
